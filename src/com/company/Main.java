953a1565056e89a18e9700679c9b032047bb392c
allanov <allanov32@gmail.com>
2022-04-25 19:15:09 +0600

    birinchi commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        //        String b = "HelLo woRld!";
+//        String b = "r!";
+//        System.out.println(a.substring(6));
+//        System.out.println(a.contains(b));
+//        System.out.println(a.toUpperCase());
+//        System.out.println(a.toLowerCase());
+//        System.out.println(a.trim());
+//        System.out.println(a.equalsIgnoreCase(b));
+//        System.out.println(a.replace("Hello", "Begaim"));
+//        System.out.println(a.length());
+//        System.out.println("Hello".concat("Joodar"));
+
+
     }
 }
